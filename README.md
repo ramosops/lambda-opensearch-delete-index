7a28bde1461c5e85d78c4990dc3b3b62dad90ca4
Diego Ramos <ramosdevops@gmail.com>
2023-05-06 22:39:01 -0300

    lambda function

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# lambda-opensearch-delete-index
+# AWS Lambda Function to Delete Old OpenSearch Indices
+
+A Python-based AWS Lambda function that deletes old indices in OpenSearch based on a user-specified age.
+
+## Table of Contents
+
+- [Features](#features)
+- [Requirements](#requirements)
+- [Installation](#installation)
+- [Configuration](#configuration)
+- [Deployment](#deployment)
+- [Contributing](#contributing)
+- [License](#license)
+- [Contact](#contact)
+
+## Features
+
+- Deletes OpenSearch indices older than a user-specified age
+- Configurable OpenSearch endpoint and index name
+- Handles retry attempts for OpenSearch operations
+
+## Requirements
+
+- An AWS account with access to Lambda and OpenSearch services
+- Python 3.x
+
+
+## Configuration
+
+To use this code, you need to update the JSON event passed to the Lambda function:
+
+1. Set the `es_endpoint` field to your OpenSearch endpoint.
+2. Set the `delete_after` field to the number of days after which indices should be deleted.
+3. Set the `index` field to the base name of the indices you want to delete, without the date.
+4. Set the `es_max_retry` field to the maximum number of retries for OpenSearch operations.
+
+## Deployment
+
+Follow these steps to deploy the Lambda function:
+
+1. Create a Lambda function in the AWS Management Console or using the AWS CLI.
+2. Upload the source code, either by zipping the files and uploading through the console, or using the AWS CLI.
+3. Set the Lambda function handler, runtime, and other configurations as needed.
+4. Add necessary triggers, such as CloudWatch events or other AWS services, to invoke the Lambda function.
+5. Test the Lambda function by passing the JSON event with the appropriate configuration.
+
+## Contributing
+
+Contributions are welcome. To contribute, please follow these steps:
+
+1. Fork the repository
+2. Create a new branch (`git checkout -b feature/YourFeature`)
+3. Commit your changes (`git commit -m 'Add some feature'`)
+4. Push to the branch (`git push origin feature/YourFeature`)
+5. Open a Pull Request
---
+++ lambda_function.py
@@ -0,0 +1,230 @@
+import datetime
+import re
+import sys
+import time
+
+import json
+import os
+from botocore.auth import SigV4Auth
+from botocore.awsrequest import AWSRequest
+from botocore.credentials import create_credential_resolver
+from botocore.httpsession import URLLib3Session
+from botocore.session import get_session
+
+if sys.version_info[0] == 3:
+    from urllib.request import quote
+else:
+    from urllib import quote
+
+
+class ES_Exception(Exception):
+    """Exception capturing status_code from Client Request"""
+    status_code = 0
+    payload = ""
+
+    def __init__(self, status_code, payload):
+        self.status_code = status_code
+        self.payload = payload
+        Exception.__init__(self,
+                           "ES_Exception: status_code={}, payload={}".format(
+                               status_code, payload))
+
+
+class ES_Cleanup(object):
+    name = "lambda_es_cleanup"
+
+    def __init__(self, event, context):
+        """Main Class init
+
+        Args:
+            event (dict): AWS Cloudwatch Scheduled Event
+            context (object): AWS running context
+        """
+        self.report = []
+        self.event = event
+        self.context = context
+
+        self.cfg = {}
+        self.cfg["es_endpoint"] = self.get_parameter("es_endpoint")
+        self.cfg["index"] = self.get_parameter("index", ".*")
+        self.cfg["skip_index"] = self.get_parameter("skip_index", ".kibana*")
+
+        self.cfg["delete_after"] = int(self.get_parameter("delete_after", 15))
+        self.cfg["es_max_retry"] = int(self.get_parameter("es_max_retry", 3))
+        self.cfg["index_format"] = self.get_parameter(
+            "index_format", "%Y.%m.%d-%H")
+
+        if not self.cfg["es_endpoint"]:
+            raise Exception("[es_endpoint] OS variable is not set")
+
+    def get_parameter(self, key_param, default_param=None):
+        """helper function to retrieve specific configuration
+
+        Args:
+            key_param     (str): key_param to read from "event" or "environment" variable
+            default_param (str): default value
+
+        Returns:
+            string: parameter value or None
+
+        """
+        return self.event.get(key_param, os.environ.get(key_param, default_param))
+
+    def send_to_es(self, path, method="GET", payload={}):
+        """Low-level POST data to Amazon Elasticsearch Service generating a Sigv4 signed request
+
+        Args:
+            path (str): path to send to ES
+            method (str, optional): HTTP method default:GET
+            payload (dict, optional): additional payload used during POST or PUT
+
+        Returns:
+            dict: json answer converted in dict
+
+        Raises:
+            #: Error during ES communication
+            ES_Exception: Description
+        """
+        if not path.startswith("/"):
+            path = "/" + path
+
+        es_region = self.cfg["es_endpoint"].split(".")[1]
+
+        headers = {
+            "Host": self.cfg["es_endpoint"],
+            "Content-Type": "application/json"
+        }
+
+        # send to ES with exponential backoff
+        retries = 0
+        while retries < int(self.cfg["es_max_retry"]):
+            if retries > 0:
+                seconds = (2 ** retries) * .1
+                time.sleep(seconds)
+
+            req = AWSRequest(
+                method=method,
+                url="https://{}{}".format(
+                    self.cfg["es_endpoint"], quote(path)),
+                data=json.dumps(payload),
+                params={"format": "json"},
+                headers=headers)
+            credential_resolver = create_credential_resolver(get_session())
+            credentials = credential_resolver.load_credentials()
+            SigV4Auth(credentials, 'es', es_region).add_auth(req)
+
+            try:
+                preq = req.prepare()
+                session = URLLib3Session()
+                res = session.send(preq)
+                if res.status_code >= 200 and res.status_code <= 299:
+                    return json.loads(res.content)
+                else:
+                    raise ES_Exception(res.status_code, res._content)
+
+            except ES_Exception as e:
+                if (e.status_code >= 500) and (e.status_code <= 599):
+                    retries += 1  # Candidate for retry
+                else:
+                    raise  # Stop retrying, re-raise exception
+
+    def delete_index(self, index_name):
+        """ES DELETE specific index
+
+        Args:
+            index_name (str): Index name
+
+        Returns:
+            dict: ES answer
+        """
+        return self.send_to_es(index_name, "DELETE")
+
+    def get_indices(self):
+        """ES Get indices
+
+        Returns:
+            dict: ES answer
+        """
+        return self.send_to_es("/_cat/indices")
+
+
+class DeleteDecider(object):
+    def __init__(self, delete_after, idx_format, idx_regex, skip_idx_regex, today):
+        self.delete_after = delete_after
+        self.idx_format = idx_format
+        self.idx_regex = idx_regex
+        self.skip_idx_regex = skip_idx_regex
+        self.today = today
+
+    def should_delete(self, index):
+        idx_split = index["index"].rsplit("-", 1 + self.idx_format.count("-"))
+        idx_date_str = '-'.join(word for word in idx_split[1:])
+        idx_name = idx_split[0]
+
+        if not re.search(self.idx_regex, index["index"]):
+            return False, "index '{}' name '{}' did not match pattern '{}'".format(index["index"],
+                                                                                   idx_name,
+                                                                                   self.idx_regex)
+        if '-' in self.idx_format:
+        # date format with hours
+            try:
+                idx_date = datetime.datetime.strptime(idx_date_str, self.idx_format)
+            except ValueError:
+                return False, "index '{}' name '{}' could not be parsed with format '{}'".format(index["index"],
+                                                                                               idx_name,
+                                                                                               self.idx_format)
+        else:
+        # date format without hours
+            idx_date_str = idx_date_str[:10]
+            try:
+                idx_date = datetime.datetime.strptime(idx_date_str, self.idx_format)
+            except ValueError:
+                return False, "index '{}' name '{}' could not be parsed with format '{}'".format(index["index"],
+                                                                                               idx_name,
+                                                                                               self.idx_format)
+
+        if re.search(self.skip_idx_regex, index["index"]):
+            return False, "index '{}' name '{}' matches skip pattern '{}'".format(index["index"],
+                                                                               idx_name,
+                                                                               self.skip_idx_regex)
+
+        earliest_to_keep = self.today - datetime.timedelta(days=self.delete_after)
+        if re.search(self.skip_idx_regex, index["index"]):
+            return False, "index matches skip condition"
+
+        try:
+            idx_datetime = datetime.datetime.strptime(idx_date_str, self.idx_format)
+            idx_date = idx_datetime.date()
+        except ValueError:
+            raise ValueError("Unable to parse index date {0} - "
+                             "incorrect index date format set?".format(idx_date_str))
+
+        if idx_date < earliest_to_keep:
+            return True, "all conditions satisfied"
+
+        return False, "deletion age of has not been reached. " \
+                      "Oldest index kept: {0}, Index Date: {1}".format(earliest_to_keep, idx_date)
+
+
+def lambda_handler(event, context):
+    """Main Lambda function
+    Args:
+        event (dict): AWS Cloudwatch Scheduled Event
+        context (object): AWS running context
+    Returns:
+        None
+    """
+    es = ES_Cleanup(event, context)
+    decider = DeleteDecider(delete_after=int(es.cfg["delete_after"]),
+                            idx_regex=es.cfg["index"],
+                            idx_format=es.cfg["index_format"],
+                            skip_idx_regex=es.cfg["skip_index"],
+                            today=datetime.date.today())
+
+    for index in es.get_indices():
+        d, reason = decider.should_delete(index)
+        if d:
+            print("Deleting index: {}".format(index["index"]))
+            es.delete_index(index["index"])
+        else:
+            print("Skipping or keeping index: {}. Reason: {}".format(index["index"], reason))
